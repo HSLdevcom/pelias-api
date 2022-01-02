b3b3bc8b397a33817012872d473bed2569b997a1
Vesa Meskanen <vesa.meskanen@cgi.com>
2022-01-02 18:25:23 +0200

    fix: do not use deprecated default value

--- query/search_defaults.js
+++ query/search_defaults.js
@@ -16,7 +16,7 @@ module.exports = _.merge({}, peliasQuery.defaults, {
 
   'boundary:rect:type': 'indexed',
 
-  'ngram:analyzer': 'peliasQueryFullToken',
+  'ngram:analyzer': 'peliasIndexOneEdgeGram',
   'ngram:field': 'name.default',
   'ngram:boost': 1,
 
