6b6bd3a9165500f27038600aa8607a9c7c47e26f
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-12-28 13:54:46 +0200

    fix lint errors

--- sanitizer/_boundary_country.js
+++ sanitizer/_boundary_country.js
@@ -13,22 +13,22 @@ function _sanitize(raw, clean) {
     // must be valid string
     if (!check.nonEmptyString(countries)) {
       messages.errors.push('boundary.country is not a string');
-      return messages
+      return messages;
     }
 
     // every item must be a valid ISO 3166 code
-    var countriesArray = countries.split(",")
+    var countriesArray = countries.split(',');
     countriesArray.forEach(country => {
       if (!containsIsoCode(country)) {
         messages.errors.push(country + ' is not a valid ISO2/ISO3 country code');
-        return messages;  
+        return messages;
       }
-    })
+    });
 
     // The codes are valid, set boundary.country to array of country codes
-    clean['boundary.country'] = countriesArray.map(country => iso3166.iso3Code(country))
+    clean['boundary.country'] = countriesArray.map(country => iso3166.iso3Code(country));
   } else { // default behaviour (set FIN)
-    clean['boundary.country'] = ["FIN"]
+    clean['boundary.country'] = ['FIN'];
   }
 
   return messages;
